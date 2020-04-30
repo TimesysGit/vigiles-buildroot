d9fa18a1c77cdd171185b4b0501f1c587e964545
Patrick Mochel <patrick.mochel@timesys.com>
2020-04-30 10:45:48 -0700

    [make] Add check for .config enabling before setting (+ formatting).

--- external.mk
+++ external.mk
@@ -4,18 +4,28 @@ vigiles-script-dir	:= $(BR2_EXTERNAL_vigiles_PATH)/scripts
 vigiles-script 		:= $(vigiles-script-dir)/$(vigiles-script-name)
 
 
+ifeq ($(VIGILES_ENABLE_KERNEL_CONFIG),y)
 vigiles-kconfig 	:= $(BR2_EXTERNAL_VIGILES_KERNEL_CONFIG)
-vigiles-kconfig 	?= none
+else
+vigiles-kconfig 	:= none
+endif
+
+ifeq ($(VIGILES_ENABLE_UBOOT_CONFIG),y)
 vigiles-uconfig 	:= $(BR2_EXTERNAL_VIGILES_UBOOT_CONFIG)
-vigiles-uconfig 	?= none
+else
+vigiles-uconfig 	:= none
+endif
+
 
 vigiles-key	:= $(BR2_EXTERNAL_VIGILES_KEY_FILE)
+
 
 ifeq ($(VIGILES_ENABLE_DASHBOARD_CONFIG),y)
 vigiles-dashboard	:= $(BR2_EXTERNAL_VIGILES_DASHBOARD_CONFIG)
 else
 vigiles-dashboard	:=
 endif
+
 
 vigiles-env = \
 	VIGILES_KEY_FILE="$(vigiles-key)" \
@@ -24,11 +34,11 @@ vigiles-env = \
 vigiles-opts = -B $(CANONICAL_CURDIR) -b $(BUILD_DIR) -o $(CANONICAL_O)
 
 ifneq ($(vigiles-kconfig),auto)
-vigiles-opts += "-k $(vigiles-kconfig)"
+vigiles-opts += -k $(vigiles-kconfig)
 endif
 
 ifneq ($(vigiles-uconfig),auto)
-vigiles-opts += "-u $(vigiles-uconfig)"
+vigiles-opts += -u $(vigiles-uconfig)
 endif
 
 
@@ -40,6 +50,7 @@ ifeq ($(VIGILES_GENERATE_INTERMEDIATE_FILES),y)
 vigiles-opts	+= --write-intermediate
 endif 	# ($(VIGILES_GENERATE_INTERMEDIATE_FILES),Y)
 endif 	# ($(VIGILES_ENABLE_EXPERT),y)
+
 
 vigiles-check:
 	@$(call MESSAGE,"Running Vigiles CVE Check")
